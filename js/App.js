03aed80a08aea6dfa4ccd68f90004c9af766cc9b
Captain122 <mfyv.fad@mail.ru>
2017-03-07 20:22:35 +0400

    22
    22

---
+++ js/App.js
@@ -0,0 +1,13 @@
+var App =React.createClass({
+    render:function(){
+        return (
+            <div className="app">
+                Hell in me
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
